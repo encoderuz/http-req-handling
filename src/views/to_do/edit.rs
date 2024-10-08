89886bb9c6275def36fee2128968c91b7f4b3cdf
encoderuz <abduqodirxojiyev@gmail.com>
2024-10-08 18:09:29 +0500

    user token logic

--- src/views/to_do/edit.rs
+++ src/views/to_do/edit.rs
@@ -2,12 +2,13 @@ use actix_web::{web, HttpResponse};
 use serde_json::{Map, Value};
 use crate::json_serialization::to_do_item::ToDoItem;
 use crate::json_serialization::to_do_items::ToDoItems;
+use crate::jwt::JwToken;
 use crate::processes::process_input;
 use crate::state::read_file;
 use crate::to_do::enums::TaskStatus;
 use crate::to_do::to_do_factory;
 
-pub async fn edit(to_do_item: web::Json<ToDoItem>) -> HttpResponse {
+pub async fn edit(to_do_item: web::Json<ToDoItem>, token: JwToken) -> HttpResponse {
     let state: Map<String, Value> = read_file("./state.json");
     let status: TaskStatus;
     match &state.get(&to_do_item.title) {
@@ -21,6 +22,7 @@ pub async fn edit(to_do_item: web::Json<ToDoItem>) -> HttpResponse {
                 format!("{} not in state", &to_do_item.title))
         }
     }
+    println!("here is the message in the token: {}",token.message);
     let existing_item = to_do_factory(to_do_item.title.as_str(), status.clone());
 
     if &status.stringify() == &TaskStatus::from_string(to_do_item.status.as_str().to_string()).stringify()
